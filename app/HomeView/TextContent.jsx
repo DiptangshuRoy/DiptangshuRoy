f5a0d719de8d23f969eabce5014cf78b199b116b
DiptangshuRoy <diptangshuroy1234@gmail.com>
2024-09-01 16:49:08 +0530

    Update TextContent.jsx

--- app/HomeView/TextContent.jsx
+++ app/HomeView/TextContent.jsx
@@ -13,11 +13,10 @@ function TextContent() {
       <h1 className="text-3xl leading-[56px] max-md:max-w-full">
         I am a Full Stack Developer_
       </h1>
-      <span className="flex items-center mt-3 leading-6 max-md:max-w-full text-center">
-        I use MERN-
-        <Image className="ml-[2px] mr-1
-                          max-2xl:hidden" src='Techicons/React.svg' height={25} width={25} alt=""></Image>
-        technologies to seamlessly build websites with both backend and frontend components.
+      <span className="mt-3 leading-6 max-md:max-w-full text-center">
+        I use MERN technologies to seamlessly build websites with both backend and frontend components.
+        {/* <Image className="ml-[2px] mr-1
+                          max-2xl:hidden" src='Techicons/React.svg' height={25} width={25} alt=""></Image> */}
       </span>
       {/* <button className="py-2.5 pr-7 pl-7 mt-3 max-w-full font-medium leading-snug border-indigo-400 border-solid bg-[linear-gradient(90deg,#7A87FB_0%,#FFD49C_100%)] border-[1.5px] min-h-[43px] rounded-[72.09px] text-neutral-900 w-[119px] max-md:px-5">
         Hire me
