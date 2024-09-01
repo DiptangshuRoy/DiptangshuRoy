02e61fdafac586f5e2d2edc4dbb69755cfbda87c
DiptangshuRoy <diptangshuroy1234@gmail.com>
2024-09-01 17:10:16 +0530

    Resized Footer LogoWithName for Mobile Device

--- app/LogoWithNameInFooterForMobile.jsx
+++ app/LogoWithNameInFooterForMobile.jsx
@@ -4,15 +4,18 @@ import React from 'react';
 function Logo() {
   return (
     <>
-      <Link href='/'>
-        <header className="flex items-center gap-3.5 text-2xl text-white whitespace-nowrap">
+      <Link href='#DiptangshuRoy'>
+        <header className="flex items-center gap-3.5 text-2xl text-white whitespace-nowrap
+                            max-md:gap-2.5">
           <img
             loading="lazy"
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/c7181cf828b5f313fb11080fbe11a068360b7c91ed2a86e5bbaba08aaae9affb?placeholderIfAbsent=true&apiKey=2385614b975c4d60b71bd5d3792c2e39"
-            className="object-contain shrink-0 my-auto aspect-[1.3] w-[39px]"
+            className="object-contain shrink-0 my-auto aspect-[1.3] w-[39px]
+                        max-md:w-6"
             alt="Portfolio logo"
           />
-          <h1 id='DiptangshuRoy' className="text-2xl text-white">
+          <h1 id='DiptangshuRoy' className="text-2xl text-white
+                                          max-md:text-base">
             Diptangshu Roy
           </h1>
         </header>
--- app/page.js
+++ app/page.js
@@ -14,6 +14,7 @@ import ClientTestimonialsCard4 from './ClientTestimonialsCards/Card4/ClientTesti
 import MyProcessOfWork from './MyProcessOfWork/MyProcessOfWork';
 import GoToTop from './GoToTopButton/GoToTopSection';
 import NavigationSidebarForMobile from './NavigationSidebarForMobile/NavigationSidebarForMobile';
+import LogoWithNameInFooterForMobile from './LogoWithNameInFooterForMobile'
 
 
 export default function Home() {
@@ -70,7 +71,8 @@ export default function Home() {
       </footer>
       <div className='m-5
                       md:hidden'>
-        <LogoWithName />
+        <LogoWithNameInFooterForMobile />
+        
       </div>
     </>
   );
