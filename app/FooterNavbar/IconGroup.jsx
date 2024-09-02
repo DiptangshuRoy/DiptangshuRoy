71c1e56df27982dedbae24824e899e8da642563b
DiptangshuRoy <diptangshuroy1234@gmail.com>
2024-09-02 20:12:13 +0530

    adjusted border-width only

--- app/FooterNavbar/IconGroup.jsx
+++ app/FooterNavbar/IconGroup.jsx
@@ -3,7 +3,7 @@ import IconItem from './IconTeam';
 
 const IconGroup = ({ icons }) => {
   return (
-    <section className="flex w-5/12 min-w-80 gap-10 justify-center items-center py-1.5 border-indigo-400 border-solid bg-zinc-900 border-[1.5px] rounded-[290px] 
+    <section className="flex w-5/12 min-w-80 gap-10 justify-center items-center py-1.5 border-2 border-indigo-400 border-solid bg-zinc-900 rounded-[290px] 
                         max-sm:gap-5 max-md:gap-5">
       {icons.map((icon, index) => (
         <IconItem key={index} src={icon.src} alt={icon.alt} />
