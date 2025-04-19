ab08a5712eb58b7b37bf2d844076086b3d21efb4
DiptangshuRoy <diptangshuroy1234@gmail.com>
2025-04-20 00:56:43 +0530

    added UI enhancements

--- src/components/FormField.tsx
+++ src/components/FormField.tsx
@@ -125,7 +125,7 @@ export default function FormField() {
           value={formData.name}
           onChange={handleChange}
           placeholder="Enter your name"
-          className="border p-2 w-full rounded-md"
+          className={`${errors.name ? "border-red-500" : "border"} border p-2 w-full rounded-md`}
         />
         {errors.name && (
           <p className="text-red-500 text-sm mt-1">{errors.name}</p>
@@ -141,7 +141,7 @@ export default function FormField() {
           value={formData.email}
           onChange={handleChange}
           placeholder="Enter your email"
-          className="border p-2 w-full rounded-md"
+          className={`${errors.email ? "border-red-500" : "border"} border p-2 w-full rounded-md`}
         />
         {errors.email && (
           <p className="text-red-500 text-sm mt-1">{errors.email}</p>
@@ -157,7 +157,7 @@ export default function FormField() {
           value={formData.message}
           onChange={handleChange}
           placeholder="Enter your message.."
-          className="border p-2 w-full rounded-md"
+          className={`${errors.message ? "border-red-500" : "border"} border p-2 w-full rounded-md`}
         />
         {errors.message && (
           <p className="text-red-500 text-sm mt-1">{errors.message}</p>
--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -111,14 +111,14 @@ export default function ProjectCard() {
               className="rounded-3xl"
             ></video> */}
             <Link className="relative" href={project.link} target="_blank" rel="noopener noreferrer">
-            <div className="absolute bottom-[16.5px] left-1/2 -translate-x-1/2 w-[96%] h-4 bg-black opacity-30 blur-md "></div>
+              <div className="absolute bottom-[16.5px] left-1/2 -translate-x-1/2 w-[96%] h-4 bg-black opacity-30 blur-md "></div>
               <Image src={project.short} height={800} width={800} alt="preview image" className="scale-110 hover:scale-115 transition cursor-pointer" />
             </Link>
           </section>
 
           <section className="flex gap-4 pt-3 lg:w-1/2">
             <div className="hidden md:block">
-              <h2 className="text-3xl lg:text-4xl">🎉</h2>
+              <h2 className="text-3xl">▸</h2>
             </div>
 
             <div className="space-y-4 lg:space-y-8">
