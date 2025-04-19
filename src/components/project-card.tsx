4104fddaccbc23a1408fdddd92bbc3b018dc5560
DiptangshuRoy <diptangshuroy1234@gmail.com>
2025-04-20 00:28:26 +0530

    added shadow under laptops

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -110,7 +110,8 @@ export default function ProjectCard() {
               playsInline
               className="rounded-3xl"
             ></video> */}
-            <Link href={project.link} target="_blank" rel="noopener noreferrer">
+            <Link className="relative" href={project.link} target="_blank" rel="noopener noreferrer">
+            <div className="absolute bottom-[16.5px] left-1/2 -translate-x-1/2 w-[96%] h-4 bg-black opacity-30 blur-md "></div>
               <Image src={project.short} height={800} width={800} alt="preview image" className="scale-110 hover:scale-115 transition cursor-pointer" />
             </Link>
           </section>
