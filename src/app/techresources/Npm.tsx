312d8b9f609abefb4d58492d85679fefd65c5b25
DiptangshuRoy <diptangshuroy1234@gmail.com>
2025-04-19 15:24:22 +0530

    just a trial commit

--- src/app/techresources/Npm.tsx
+++ src/app/techresources/Npm.tsx
@@ -34,7 +34,7 @@ export function Npm() {
       <CardContent>
         <CardTitle className="text-2xl">NPM</CardTitle>
         <CardDescription>
-          Popular React framework for building server-side rendered and static web applications.
+          sfsfasfsfasf
         </CardDescription>
       </CardContent>
       <CardFooter className="space-x-4">
